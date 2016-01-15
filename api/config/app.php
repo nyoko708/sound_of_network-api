1758e3de536d509b22aa3e041e4a92565da5e0fa
nyoko708 <ckgxd325@yahoo.co.jp>
2016-01-15 03:35:37 +0000

    edit

--- api/config/app.php
+++ api/config/app.php
@@ -156,6 +156,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * JWTAuth
+         */
+        //Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class
     ],
 
     /*
@@ -202,6 +206,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
 
+//        'JWTAuth'   => Tymon\JWTAuth\Facades\JWTAuth::class,
+//        'JWTFactory'=> Tymon\JWTAuth\Facades\JWTFactory::class
     ],
 
 ];
