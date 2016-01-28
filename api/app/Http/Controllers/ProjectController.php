db191a2fc32dbe28a7144072a270dc083eaf0320
nyoko708 <ckgxd325@yahoo.co.jp>
2016-01-28 04:34:33 +0000

    edit Project Controller

---
+++ api/app/Http/Controllers/ProjectController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ProjectController extends Controller
+{
+  /**
+   * Construct
+   */
+  public function __construct()
+  {
+    $this->middleware('jwt.auth', ['except' => ['get']]);
+  }
+
+  /**
+   * /project/{projectId?} API
+   *
+   * @desc プロジェクトの情報取得API
+   * @param int $projectId : プロジェクトID NULLの場合は、リストで返す
+   * @return array
+   */
+  public function get($projectId = null)
+  {
+    if(!is_null($projectId)){
+      return response()->json(['project' => $projectId]);
+    } else {
+      return response()->json(['project' => 'list']);
+    }
+  }
+}
--- api/app/Http/routes.php
+++ api/app/Http/routes.php
@@ -40,13 +40,27 @@ Route::group(['prefix' => 'api'], function () {
     return Response::json("test");
   });
 
+
+  // ------------------ //
+  // --- 認証系 API --- //
+  // ------------------ //
   // 認証情報取得
   Route::get('/authenticate', 'AuthenticateController@get');
 
   // 認証処理
   Route::post('/authenticate', 'AuthenticateController@auth');
 
-  // ユーザー関連
+
+  // ---------------------- //
+  // --- ユーザー系 API --- //
+  // ---------------------- //
   Route::get('/user', 'UserController@get');
   Route::post('/user', 'UserController@create');
+
+
+  // -------------------------- //
+  // --- プロジェクト系 API --- //
+  // -------------------------- //
+  Route::get('/project/{projectId?}', 'ProjectController@get')->where('projectId', '[0-9]+');
+
 });
